1547c550f40777f08915d23429f7639880bae6ca
Smruti Tripathy <smruti.tripathy@capgemini.com>
2016-03-08 20:48:42 +0530

    Final Commit

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+yum update -y --exclude=kernel
+yum install -y nano git unzip screen
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+service httpd start
+
+# PHp
+yum install -y php php-cli php-common php-devel php-mysql
+
+#Mysql
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+
+# Download Starter Content
+
+service httpd restart 
+
